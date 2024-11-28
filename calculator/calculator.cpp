c6e1b8ed0ba3006d8b7e1354c33427f943a67b42
Garegin866 <gareginharutyunyan866@gmail.com>
2024-11-28 23:45:10 +0400

    Реализована базовая функциональность калькулятора.

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,87 @@
-// В этом файле определения функций.
-// За основу возьмите решение предыдущей задачи.
+#include "calculator.h"
+
+#include <iostream>
+#include <cmath>
+
+using namespace std::literals;
+
+bool ReadNumber(Number& result) {
+    const auto err_not_num = "Error: Numeric operand expected"s;
+
+    if (!(std::cin >> result)) {
+        std::cerr << err_not_num << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool RunCalculatorCycle() {
+    Number number;
+    ReadNumber(number);
+
+    std::optional<double> buffer;
+    std::string token;
+    while(std::cin >> token) {
+        if (token == "q"s) {
+            return true;
+        } else if (token == "+"s || token == "-"s || token == "*"s || token == "/"s || token == "**"s) {
+            if (!PerformArithmeticOperation(number, token)) {
+                break;
+            }
+        } else {
+            if (!PerformControlOperation(number, token, buffer)) {
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+bool PerformArithmeticOperation(Number& number, std::string token) {
+    Number right;
+    if (!ReadNumber(right)) {
+        return false;
+    }
+
+    if (token == "+"s) {
+        number += right;
+    } else if (token == "-"s) {
+        number -= right;
+    } else if (token == "*"s) {
+        number *= right;
+    } else if (token == "/"s) {
+        number /= right;
+    } else if (token == "**"s) {
+        number = std::pow(number, right);
+    } else {
+        std::cerr << "Error: Unknown token "s << token << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool PerformControlOperation(Number& number, std::string token, std::optional<double>& buffer) {
+    if (token == "c"s) {
+        number = 0;
+    } else if (token == "s"s) {
+        buffer = number;
+    } else if (token == "="s) {
+        std::cout << number << std::endl;
+    } else if (token == ":"s) {
+        Number right;
+        if (!ReadNumber(right)) {
+            return false;
+        }
+        number = right;
+    } else if (token == "l"s) {
+        if (!buffer.has_value()) {
+            std::cerr << "Error: Memory is empty " << std::endl;
+            return false;
+        }
+        number = buffer.value();
+    } else {
+        std::cerr << "Error: Unknown token "s << token << std::endl;
+        return false;
+    }
+    return true;
+}
--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,1 +1,23 @@
-// В этом файле объявления функций.
+#pragma once
+
+#include <optional>
+#include <string>
+
+using Number = double;
+
+// Возвращает true, если удалось прочитать число
+// и сохранить его в Number.
+bool ReadNumber(Number& result);
+
+// Возвращает true, если работа завершилась штатно
+// командой q. Если из-за ошибки, возвращает false.
+bool RunCalculatorCycle();
+
+// Возвращает true, если удалось выполнить арифметическую операцию
+// и сохранить результат в number.
+// В случае ошибки возвращает false.
+bool PerformArithmeticOperation(Number& number, std::string token);
+
+// Возвращает true, если удалось выполнить управляющую операцию.
+// В случае ошибки возвращает false.
+bool PerformControlOperation(Number& number, std::string token, std::optional<double>& buffer);
